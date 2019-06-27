b23aed2b8fe1b6a96f7836ebdaf9220c22e533a0
Derick Phan <dtph@ucdavis.edu>
2019-06-27 00:23:39 -0700

    added auth backend

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 var db = require('./database');
 
 const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
 
+var auth = require('./auth');
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+app.use('/auth', auth);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
@@ -20,6 +26,14 @@ db.query('SELECT NOW()', (err, res) => {
   if (err.error)
     return console.log(err.error);
   console.log(`PostgreSQL connected: ${res[0].now}.`);
+});
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  });
 });
 
 module.exports = app;
